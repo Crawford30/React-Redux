d33e8d965ca545d8c5dcdc2acc0c2ac3870f1af9
Joel Crawford <macbook@Macbooks-MacBook-Pro.local>
2022-08-31 11:02:51 +0300

    Iterating over a list

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,12 +2,22 @@ import React, {Component} from 'react';
 import Ninjas from './Ninjas';
 
 class  App extends Component {
+  state = {
+    ninjas: [
+      {name: 'Joel', age: 30, belt: 'black', id: 1},
+      {name: 'John', age: 20, belt: 'brown', id: 2},
+      {name: 'Peter', age: 35, belt: 'pink', id: 3},
+      {name: 'Sam', age: 40, belt: 'red', id: 4},
+      {name: 'Okoth', age: 6, belt: 'green', id: 5},
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My First React App</h1>
         <p>Welcome</p>
-        <Ninjas name="Joel" age="13" belt="black"></Ninjas>
+        {/* <Ninjas name="Joel" age="13" belt="black"></Ninjas> */}
+        <Ninjas ninjas={this.state.ninjas}></Ninjas>
       </div>
     );
 
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -3,20 +3,42 @@ import React, {Component} from 'react';
 class Ninjas extends Component {
     render() {
         const {name, age, belt} = this.props;
+        // const ninjas = this.props.ninjas;
+
+        const {ninjas} = this.props;
+
         console.log(this.props);
-        return (
-            <div className="ninjas">
 
-                <div>Name: {name}</div>
-                <div>Age: {age}</div>
-                <div>Belt: {belt}</div>
-
-                {/* <div>Name: {this.props.name}</div>
-                <div>Age: {this.props.age}</div>
-                <div>Belt: {this.props.belt}</div> */}
+        const ninjaList = ninjas.map(ninja => {
+            return (
+                <div className="ninjas" key={ninja.id}>
+                <div>Name: {ninja.name}</div>
+                <div>Age: {ninja.age}</div>
+                <div>Belt: {ninja.belt}</div>
             </div>
 
-        );
+            )
+        })
+
+
+        return (
+            <div className="list-item">
+                {ninjaList}
+            </div>
+        )
+        // return (
+        //     <div className="ninjas">
+
+        //         <div>Name: {name}</div>
+        //         <div>Age: {age}</div>
+        //         <div>Belt: {belt}</div>
+
+        //         {/* <div>Name: {this.props.name}</div>
+        //         <div>Age: {this.props.age}</div>
+        //         <div>Belt: {this.props.belt}</div> */}
+        //     </div>
+
+        // );
     }
 }
 
