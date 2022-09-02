556bcc83a314ec4659f70c15f7e2cb1ff30c8f38
Joel Crawford <macbook@Crawford.local>
2022-09-02 15:00:10 +0300

    Delete Ninja

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -13,20 +13,46 @@ class  App extends Component {
     ]
   }
 
+  //Add Action
   addNinja = (ninja) => {
     console.log("NINJA OBJECT: ", ninja);
 
+    ninja.id = Math.random();
+    let newNinjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: newNinjas
+    });
+  
   } 
+
+  //Delete Action
+  deleteNinja = (id) => {
+    console.log("ID: ", id);
+
+    //we can use a non destructuring method, filter
+
+    let newNinjas = this.state.ninjas.filter(ninja => {
+
+      //return false if the id is equal to the ninja.id
+      return ninja.id !== id;  //this will return false if id === ninja.id and will filter the ninja out of the array
+    });
+
+    this.setState({
+      ninjas:newNinjas 
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
         <h1>My First React App</h1>
         <p>Welcome</p>
         {/* <Ninjas name="Joel" age="13" belt="black"></Ninjas> */}
-        <Ninjas ninjas={this.state.ninjas}></Ninjas>
+        <Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja} ></Ninjas>
 
           {/*Send the addNinja function as prop to addNinja component */}
-        <AddNinja addNinja={this.addNinja}/>  
+        <AddNinja addNinja={this.addNinja} />  
       </div>
     );
 
