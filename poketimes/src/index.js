62abc30e3ef5895a3d3b65794e2f62093ec0e3dd
Joel Crawford <macbook@Crawford.local>
2022-09-10 20:22:59 +0300

    Redux::Reducer files and store

--- poketimes/src/index.js
+++ poketimes/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './reducers/rootReducer';
+
+//Redux
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+  ;
+const store = createStore(rootReducer);
+
+//Provider tag is wrapping our App tag to expose the store globally
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
