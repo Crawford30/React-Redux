3ca7665db7edd5372aa9a5f69b0e1e7825239dc9
Joel Crawford <macbook@Crawford.local>
2022-09-10 15:40:09 +0300

    add single post

--- poketimes/src/components/SinglePost.js
+++ poketimes/src/components/SinglePost.js
@@ -1,24 +1,46 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 
 class SinglePost extends Component {
     state = {
-        id: null
+        post: null
     }
 
     componentDidMount() {
         console.log("SINGLE POST PROPS: ", this.props); //we automatically props in class based component from the route
-        let id = this.props.match.params.post_id;
-        this.setState({
-            id: id
-        })
+
+        //let id = this.props.match.params.post_id;
+        // this.setState({
+        //     id: id
+        // })
+
+        axios.get('https://jsonplaceholder.typicode.com/posts/2')
+            .then(res => {
+                this.setState({
+                    post: res.data
+                })
+
+                console.log("RESPOND FROM SINGLE POST API ", res);
+            })
+
+
 
     }
 
     render() {
+        const post = this.state.post ? (
+            <div className="post">
+                <h4 className="center">{this.state.post.title}</h4>
+                <p>{this.state.post.body}</p>
+
+            </div>
+        ) : (
+            <div className="center">Loading post...</div>
+        )
         return (
             <div className="container">
-                <h4>Route Parameter: {this.state.id}</h4>
+                {post}
             </div>
         )
     }
