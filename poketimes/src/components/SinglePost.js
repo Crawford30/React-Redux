8e163b9f12185b3bb33706ed0037849d4f6b0913
Joel Crawford <macbook@Crawford.local>
2022-09-11 10:51:36 +0300

    mapping state to props

--- poketimes/src/components/SinglePost.js
+++ poketimes/src/components/SinglePost.js
@@ -1,38 +1,43 @@
 import React, { Component } from "react";
 import axios from 'axios';
 
+//connect component to redux
+import { connect } from 'react-redux';
+
 
 class SinglePost extends Component {
-    state = {
-        post: null
-    }
+    // state = {
+    //     post: null
+    // }
 
-    componentDidMount() {
-        console.log("SINGLE POST PROPS: ", this.props); //we automatically props in class based component from the route
+    // componentDidMount() {
+    //     console.log("SINGLE POST PROPS: ", this.props); //we automatically props in class based component from the route
 
-        //let id = this.props.match.params.post_id;
-        // this.setState({
-        //     id: id
-        // })
+    //     //let id = this.props.match.params.post_id;
+    //     // this.setState({
+    //     //     id: id
+    //     // })
 
-        axios.get('https://jsonplaceholder.typicode.com/posts/2')
-            .then(res => {
-                this.setState({
-                    post: res.data
-                })
+    //     axios.get('https://jsonplaceholder.typicode.com/posts/2')
+    //         .then(res => {
+    //             this.setState({
+    //                 post: res.data
+    //             })
 
-                console.log("RESPOND FROM SINGLE POST API ", res);
-            })
-
-
-
-    }
+    //             console.log("RESPOND FROM SINGLE POST API ", res);
+    //         })
+    // }
 
     render() {
-        const post = this.state.post ? (
+        // const post = this.state.post ? (
+
+        const post = this.props.post ? (
             <div className="post">
-                <h4 className="center">{this.state.post.title}</h4>
-                <p>{this.state.post.body}</p>
+                {/* <h4 className="center">{this.state.post.title}</h4>
+                <p>{this.state.post.body}</p> */}
+
+                <h4 className="center">{this.props.post.title}</h4>
+                <p>{this.props.post.body}</p>
 
             </div>
         ) : (
@@ -46,5 +51,23 @@ class SinglePost extends Component {
     }
 
 }
+//function to map state to props
 
-export default SinglePost
+const mapStateToProps = (state, ownProps) => { //we take a second parameter called ownProps
+
+    //ownProps have info about the post from the rout
+    let id = 1;
+    //ownProps.match.params.post_id;
+
+    //we retrun the object
+    return {
+        // post: state.posts.find((post) => {
+        //     return post.id === id 
+        // }) is the same as one below
+
+        post: state.posts.find(post => post.id === id)
+    }
+
+}
+
+export default connect(mapStateToProps)(SinglePost)
